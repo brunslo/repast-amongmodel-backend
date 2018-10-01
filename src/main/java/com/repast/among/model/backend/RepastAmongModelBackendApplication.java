f26c71d71ef956ffe6cb0489d32e7e7ab4b367fa
Sadia Waleem <waleems@4180L-144235-M.local>
2018-10-01 22:24:30 +1000

    Spring boot repast among backend app structure

---
+++ src/main/java/com/repast/among/model/backend/RepastAmongModelBackendApplication.java
@@ -0,0 +1,12 @@
+package com.repast.among.model.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepastAmongModelBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepastAmongModelBackendApplication.class, args);
+    }
+}
