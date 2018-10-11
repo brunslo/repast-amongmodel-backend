1314cc86479707ad5f8be3ff986917354ebd4fdc
Sadia Waleem <waleems@4180L-144235-M.local>
2018-10-11 11:25:18 +1100

    refactor code

--- src/main/java/com/repast/among/model/backend/controllers/AmongModelController.java
+++ src/main/java/com/repast/among/model/backend/controllers/AmongModelController.java
@@ -5,9 +5,7 @@ import com.repast.among.model.backend.services.AmongModelService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +16,6 @@ public class AmongModelController {
 
     @NonNull
     private final AmongModelService amongModelService;
-
 
     @GetMapping("/getInitialParam")
     public ResponseEntity<?> getInitialParam() {
