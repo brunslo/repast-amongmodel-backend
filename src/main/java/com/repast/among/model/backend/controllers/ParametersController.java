1e8573ad0b9a1235ce1fdecf8331c8834cd821b0
Sadia Waleem <waleems@4180L-144235-M.local>
2018-10-25 14:01:02 +1100

    Add Get and Post endpoints for chart data

--- src/main/java/com/repast/among/model/backend/controllers/ParametersController.java
+++ src/main/java/com/repast/among/model/backend/controllers/ParametersController.java
@@ -3,12 +3,18 @@ package com.repast.among.model.backend.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
 
 @RestController
-public class ParametersController {
+public class ParametersController{
     private final Map<String, String> parameterMap = new ConcurrentHashMap<>();
+    private final List<Object> allChartMap = Collections.synchronizedList(new ArrayList<>());
 
     @PostMapping("/initialise")
     public ResponseEntity<?> initialise(@RequestBody final Map<String, String> parameterMap) {
@@ -29,4 +35,17 @@ public class ParametersController {
     public ResponseEntity<?> parameters() {
         return ResponseEntity.ok(parameterMap);
     }
+
+    @PostMapping("/sendCharts")
+    public ResponseEntity<?> sendCharts(@RequestBody final Map<String, java.lang.String> chartMap) {
+
+        this.allChartMap.add(chartMap);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/getCharts")
+    public ResponseEntity<?> getCharts() {
+        return ResponseEntity.ok(allChartMap);
+    }
+
 }
